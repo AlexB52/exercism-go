a70a11d7441378803ef8ebb44be15edf67e194ee
Alexandre Barret <alex@abletech.nz>
2021-09-12 17:28:48 +1200

    Solve the exercise

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -2,22 +2,62 @@ package blackjack
 
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
-	panic("Please implement the ParseCard function")
+	switch card {
+	case "ace":
+		return 11
+	case "two":
+		return 2
+	case "three":
+		return 3
+	case "four":
+		return 4
+	case "five":
+		return 5
+	case "six":
+		return 6
+	case "seven":
+		return 7
+	case "eight":
+		return 8
+	case "nine":
+		return 9
+	default:
+		return 10
+	}
 }
 
 // IsBlackjack returns true if the player has a blackjack, false otherwise.
 func IsBlackjack(card1, card2 string) bool {
-	panic("Please implement the IsBlackjack function")
+	return 21 == ParseCard(card1) + ParseCard(card2)
 }
 
 // LargeHand implements the decision tree for hand scores larger than 20 points.
 func LargeHand(isBlackjack bool, dealerScore int) string {
-	panic("Please implement the LargeHand function")
+	if isBlackjack {
+		switch dealerScore {
+		case 10, 11:
+			return "S"
+		default:
+			return "W"
+		}
+	}
+
+	return "P"
 }
 
 // SmallHand implements the decision tree for hand scores with less than 21 points.
 func SmallHand(handScore int, dealerScore int) string {
-	panic("Please implement the SmallHand function")
+	if handScore >= 17 {
+		return "S"
+	} else if handScore <= 11 {
+		return "H"
+	} else {
+		if dealerScore >= 7 {
+			return "H"
+		}
+
+		return "S"
+	}
 }
 
 // FirstTurn returns the semi-optimal decision for the first turn, given the cards of the player and the dealer.
