a2fba4f14c42cee2bd10d700bfa0039fc2163f66
Alexandre Barret <alex@abletech.nz>
2021-09-19 16:52:56 +1200

    Solve logs, logs, logs

--- logs-logs-logs/logs.go
+++ logs-logs-logs/logs.go
@@ -1,21 +1,34 @@
 package logs
 
+import (
+	"fmt"
+	"strings"
+	"regexp"
+)
+
+func parseLog(line string) []string {
+	re := regexp.MustCompile(`\[(.*)\]:(.*)`)
+	return re.FindStringSubmatch(line)
+}
+
 // Message extracts the message from the provided log line.
 func Message(line string) string {
-	panic("Please implement the Message() function")
+	match := parseLog(line)
+	return strings.TrimSpace(match[2])
 }
 
 // MessageLen counts the amount of characters (runes) in the message of the log line.
 func MessageLen(line string) int {
-	panic("Please implement the MessageLen() function")
+	return len([]rune(Message(line)))
 }
 
 // LogLevel extracts the log level string from the provided log line.
 func LogLevel(line string) string {
-	panic("Please implement the LogLevel() function")
+	match := parseLog(line)
+	return strings.ToLower(match[1])
 }
 
 // Reformat reformats the log line in the format `message (logLevel)`.
 func Reformat(line string) string {
-	panic("Please implement the Reformat() function")
+	return fmt.Sprintf("%s (%s)", Message(line), LogLevel(line))
 }
