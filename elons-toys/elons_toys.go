2ba310d0b5f7197278e185bff021decef4027155
Alexandre Barret <alex@abletech.nz>
2021-09-20 08:51:07 +1200

    Solve elon toys

--- elons-toys/elons_toys.go
+++ elons-toys/elons_toys.go
@@ -1,5 +1,7 @@
 package elon
 
+import ("fmt")
+
 // Car implements a remote controlled car.
 type Car struct {
 	speed        int
@@ -16,30 +18,33 @@ type Track struct {
 
 // CreateCar creates a new car with given specifications.
 func CreateCar(speed, batteryDrain int) *Car {
-	panic("Please implement CreateCar() method")
+	return &Car {speed: speed, battery: 100, batteryDrain: batteryDrain}
 }
 
 // CreateTrack creates a new track with given distance.
 func CreateTrack(distance int) Track {
-	panic("Please implement CreateTrack() method")
+	return Track {distance}
 }
 
 // Drive drives the car one time.
 func (car *Car) Drive() {
-	panic("Please implement Drive() method")
+	car.battery -= car.batteryDrain
+	car.distance += car.speed
 }
 
 // CanFinish checks if a car is able to finish a certain track.
 func (car *Car) CanFinish(track Track) bool {
-	panic("Please implement CanFinish() method")
+	drivesRequired  := track.distance / car.speed
+	drivesAvailable := car.battery / car.batteryDrain
+	return drivesRequired <= drivesAvailable
 }
 
 // DisplayDistance displays the distance the car is driven.
 func (car *Car) DisplayDistance() string {
-	panic("Please implement DisplayDistance() method")
+	return fmt.Sprintf("Driven %d meters", car.distance)
 }
 
 // DisplayBattery displays the battery level.
 func (car *Car) DisplayBattery() string {
-	panic("Please implement DisplayBattery() method")
+	return fmt.Sprintf("Battery at %d%%", car.battery)
 }
