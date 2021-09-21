6428c13179890ca810a73c9ad57dee8a75eb2609
Alexandre Barret <alex@abletech.nz>
2021-09-21 16:55:59 +1200

    WIP - solve exercise

--- weather-forecast/weather.go
+++ weather-forecast/weather.go
@@ -1,8 +1,13 @@
+// Package: provides a function to display the weather forecast
 package weather
 
+// this is the current condition of the User
 var CurrentCondition string
+
+// this is the current location of the User
 var CurrentLocation string
 
+// Forecast formats the forecast message
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
