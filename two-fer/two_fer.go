64f21cf0a7c2ad74646f95f7479aa529c331d8e2
Alexandre Barret <alex@abletech.nz>
2021-09-21 17:06:04 +1200

    Solve two-fer exercise

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,11 +1,8 @@
 package twofer
 
-import ("fmt")
+import "fmt"
 
 func ShareWith(name string) string {
-	if (name == 0) {
-        return "One for you, one for me."
-    }
-
-	return fmt.Sprintf("One for %s, one for me.", name)	
+	if (name == "") { name = "you" }
+	return fmt.Sprintf("One for %s, one for me.", name)
 }
