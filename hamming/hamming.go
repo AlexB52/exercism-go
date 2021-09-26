ace46506ce14de461aa5116afb0037d8a1f4bf97
Alexandre Barret <alex@abletech.nz>
2021-09-26 16:47:34 +1300

    Format hamming solution

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -3,15 +3,15 @@ package hamming
 import "errors"
 
 func Distance(a, b string) (distance int, er error) {
-	if (len(a) != len(b)) {
+	if len(a) != len(b) {
 		return 0, errors.New("a & b of different length")
- 	}
+	}
 
- 	for index, _ := range a {
- 		if ( a[index] != b[index] ) {
- 			distance ++
- 		}
- 	}
+	for index := range a {
+		if a[index] != b[index] {
+			distance++
+		}
+	}
 
- 	return
+	return
 }
