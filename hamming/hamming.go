08ed258dd46c2d782af02c21db4cadbe6ff224ef
Alexandre Barret <alex@abletech.nz>
2021-09-26 16:45:29 +1300

    Solve hamming

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,4 +1,17 @@
 package hamming
 
-func Distance(a, b string) (int, error) {
+import "errors"
+
+func Distance(a, b string) (distance int, er error) {
+	if (len(a) != len(b)) {
+		return 0, errors.New("a & b of different length")
+ 	}
+
+ 	for index, _ := range a {
+ 		if ( a[index] != b[index] ) {
+ 			distance ++
+ 		}
+ 	}
+
+ 	return
 }
