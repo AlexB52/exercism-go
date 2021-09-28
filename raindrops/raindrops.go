ce4e205581fa9559f084830cadfcf60880341b24
Alexandre Barret <alex@abletech.nz>
2021-09-28 17:19:13 +1300

    go fmt

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -5,15 +5,15 @@ import "fmt"
 func Convert(num int) string {
 	result := ""
 
-	if (num % 3 == 0) {
+	if num%3 == 0 {
 		result += "Pling"
 	}
 
-	if (num % 5 == 0) {
+	if num%5 == 0 {
 		result += "Plang"
 	}
 
-	if (num % 7 == 0) {
+	if num%7 == 0 {
 		result += "Plong"
 	}
 
