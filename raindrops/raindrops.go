db366ca56c51da4a88f786a77609283418e77f46
Alexandre Barret <alex@abletech.nz>
2021-09-28 17:18:42 +1300

    Solve raindrops

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,1 +1,25 @@
 package raindrops
+
+import "fmt"
+
+func Convert(num int) string {
+	result := ""
+
+	if (num % 3 == 0) {
+		result += "Pling"
+	}
+
+	if (num % 5 == 0) {
+		result += "Plang"
+	}
+
+	if (num % 7 == 0) {
+		result += "Plong"
+	}
+
+	if len(result) > 0 {
+		return result
+	} else {
+		return fmt.Sprintf("%d", num)
+	}
+}
