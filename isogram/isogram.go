55cbfbb1c225cde0bb907b1ad44be8d7d0a62e2a
Alexandre Barret <alex@abletech.nz>
2021-10-08 11:58:33 +1300

    Solve isogram

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,1 +1,20 @@
 package isogram
+
+import (
+	"strings"
+)
+
+func IsIsogram(word string) bool {
+	letterCount := make(map[rune]int)
+	trimedWord := strings.ReplaceAll(strings.ReplaceAll(strings.ToLower(word), "-", ""), " ", "")
+
+	for _, letter := range trimedWord {
+		letterCount[letter]++
+
+		if letterCount[letter] > 1 {
+			return false
+		}
+	}
+
+	return true
+}
