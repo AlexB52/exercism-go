d17c4ea0e321814c21f4bad820bf02c074e841d8
Alexandre Barret <alex@abletech.nz>
2021-10-09 17:36:28 +1300

    Solve difference of squares

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,1 +1,21 @@
 package diffsquares
+
+func SquareOfSum(n int) (result int) {
+	for i := 0; i <= n; i++ {
+		result += i
+	}
+
+	return result * result
+}
+
+func SumOfSquares(n int) (result int) {
+	for i := 0; i <= n; i++ {
+		result += i * i
+	}
+
+	return result
+}
+
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
