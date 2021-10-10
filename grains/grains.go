6d4b89e8bd71a9bbe04ceb33af2175fafbd2af05
Alexandre Barret <alex@abletech.nz>
2021-10-11 08:47:40 +1300

    Solve grains

--- grains/grains.go
+++ grains/grains.go
@@ -1,1 +1,21 @@
 package grains
+
+import (
+	"math"
+	"errors"
+)
+
+func Square(number int) (uint64, error) {
+	if number < 1 || number > 64 {
+		return 0, errors.New("number is not within a valid range of [1, 64]")
+	}
+
+	return uint64(math.Exp2(float64(number -1))), nil
+
+}
+
+func Total() uint64 {
+	// total number of grains on a board of 64 squares
+	// 2**0 + 2**1 + ... + 2**63
+	return uint64(18446744073709551615)
+}
