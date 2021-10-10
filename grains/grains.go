1b14e88e4a2836ae821fefb6bedcb1358298d978
Alexandre Barret <alex@abletech.nz>
2021-10-11 09:03:16 +1300

    Refactor grain solution from community solutions

--- grains/grains.go
+++ grains/grains.go
@@ -10,12 +10,11 @@ func Square(number int) (uint64, error) {
 		return 0, errors.New("number is not within a valid range of [1, 64]")
 	}
 
-	return uint64(math.Exp2(float64(number - 1))), nil
-
+	return 1 << (number - 1), nil
 }
 
 func Total() uint64 {
 	// total number of grains on a board of 64 squares
 	// 2**0 + 2**1 + ... + 2**63
-	return uint64(18446744073709551615)
+	return math.MaxUint64
 }
