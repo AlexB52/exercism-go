1acb980f2581f85655ed104f58d476a610ec8a89
Alexandre Barret <alex@abletech.nz>
2021-10-18 16:43:01 +1300

    Solve clock

--- clock/clock.go
+++ clock/clock.go
@@ -1,1 +1,46 @@
 package clock
+
+import "fmt"
+
+type Clock struct {
+	hour, minute int
+}
+
+func New(hour, minute int) Clock {
+	hour, minute = ConvertMinutes(hour*60 + minute)
+	return Clock{hour, minute}
+}
+
+func ConvertMinutes(minutes int) (hour, minute int) {
+	hour = minutes / 60 % 24
+	minute = minutes % 60
+
+	if minute < 0 {
+		minute += 60
+		hour--
+	}
+
+	if hour < 0 {
+		hour += 24
+	}
+
+	return hour, minute
+}
+
+func (clock Clock) String() string {
+	return fmt.Sprintf("%02d:%02d", clock.hour, clock.minute)
+}
+
+func (clock Clock) TotalMinutes() int {
+	return clock.hour*60 + clock.minute
+}
+
+func (clock Clock) Add(minutes int) Clock {
+	clock.hour, clock.minute = ConvertMinutes(clock.TotalMinutes() + minutes)
+	return clock
+}
+
+func (clock Clock) Subtract(minutes int) Clock {
+	clock.hour, clock.minute = ConvertMinutes(clock.TotalMinutes() - minutes)
+	return clock
+}
