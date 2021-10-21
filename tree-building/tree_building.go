be940f7224f26de8a2c8d526aee478af6dfdf072
Alexandre Barret <alex@abletech.nz>
2021-10-21 20:19:43 +1300

    Solve the first part of the exercise

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -1,1 +1,46 @@
 package tree
+
+import (
+	"sort"
+)
+
+type Record struct {
+	ID, Parent int
+}
+
+type Node struct {
+	ID       int
+	Children []*Node
+}
+
+func Build(records []Record) (*Node, error) {
+	if len(records) == 0 {
+		return nil, nil
+	}
+
+	sort.Slice(records, RecordSortingMethod(records))
+	return BuildNode(records, &Node{ID: 0}), nil
+}
+
+func BuildNode(records []Record, node *Node) *Node {
+	for _, record := range records {
+		if node.ID == record.ID {
+			continue
+		}
+		if node.ID != record.Parent {
+			continue
+		}
+
+		newNode := &Node{ID: record.ID}
+		BuildNode(records, newNode)
+		node.Children = append(node.Children, newNode)
+	}
+
+	return node
+}
+
+func RecordSortingMethod(records []Record) func(i, j int) bool {
+	return func(i, j int) bool {
+		return records[i].ID < records[j].ID
+	}
+}
