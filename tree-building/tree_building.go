3094651e495741ec9fc34eb9563cbdfa6e0d52bd
Alexandre Barret <alex@abletech.nz>
2021-10-25 09:19:55 +1300

    Refactor solution

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -16,13 +16,13 @@ type Node struct {
 }
 
 func Build(records []Record) (*Node, error) {
-	SortRecords(records)
+	sort.Slice(records, func(i, j int) bool { return records[i].ID < records[j].ID })
 
 	if len(records) == 0 {
 		return nil, nil
 	}
 
-	if ok, err := ValidateRootNode(records[0]); !ok {
+	if ok, err := ValidateRootRecord(records[0]); !ok {
 		return nil, err
 	}
 
@@ -42,19 +42,7 @@ func Build(records []Record) (*Node, error) {
 	return nodes[0], nil
 }
 
-func ValidateRecord(index int, record Record) (bool, error) {
-	if index != record.ID {
-		return false, fmt.Errorf("non-contiguous record ID: %+v", record)
-	}
-
-	if record.Parent != 0 && record.ID <= record.Parent {
-		return false, fmt.Errorf("higher parent id than record id: %+v", record)
-	}
-
-	return true, nil
-}
-
-func ValidateRootNode(record Record) (bool, error) {
+func ValidateRootRecord(record Record) (bool, error) {
 	if record.Parent != 0 {
 		return false, errors.New("root node has parent")
 	}
@@ -66,8 +54,14 @@ func ValidateRootNode(record Record) (bool, error) {
 	return true, nil
 }
 
-func SortRecords(records []Record) {
-	sort.Slice(records, func(i, j int) bool {
-		return records[i].ID < records[j].ID
-	})
+func ValidateRecord(index int, record Record) (bool, error) {
+	if index != record.ID {
+		return false, fmt.Errorf("non-contiguous record ID: %+v", record)
+	}
+
+	if record.Parent != 0 && record.ID <= record.Parent {
+		return false, fmt.Errorf("higher parent id than record id: %+v", record)
+	}
+
+	return true, nil
 }
