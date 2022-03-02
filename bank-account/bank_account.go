f633f2825553820e6090b655d94052fae78067ce
Alexandre Barret <alexandre@hatchinvest.nz>
2022-03-03 09:19:10 +1300

    Solve bank account exercise

--- bank-account/bank_account.go
+++ bank-account/bank_account.go
@@ -1,19 +1,57 @@
 package account
 
+import "sync"
+
 // Define the Account type here.
 
+type Account struct {
+	mu      sync.Mutex
+	open    bool
+	balance int64
+}
+
 func Open(amount int64) *Account {
-	panic("Please implement the Open function")
+	if amount < 0 {
+		return nil
+	}
+
+	return &Account{open: true, balance: amount}
 }
 
 func (a *Account) Balance() (int64, bool) {
-	panic("Please implement the Balance function")
+	if !a.open {
+		return 0, false
+	}
+
+	if a.balance >= 0 {
+		return a.balance, true
+	} else {
+		return 0, false
+	}
 }
 
 func (a *Account) Deposit(amount int64) (int64, bool) {
-	panic("Please implement the Deposit function")
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
+	if !a.open || a.balance+amount < 0 {
+		return 0, false
+	}
+
+	a.balance += amount
+	return a.balance, true
 }
 
 func (a *Account) Close() (int64, bool) {
-	panic("Please implement the Close function")
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
+	if !a.open {
+		return 0, false
+	}
+
+	payout := a.balance
+	a.balance -= payout
+	a.open = false
+	return payout, true
 }
