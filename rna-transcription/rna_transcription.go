75e2348b26b69d4d926f5a547b3083cde7349d36
Alexandre Barret <alexandre@hatchinvest.nz>
2022-03-19 18:14:22 +1300

    Solve rna-transcription

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,5 +1,11 @@
 package strand
 
-func ToRNA(dna string) string {
-	panic("Please implement the ToRNA function")
+func ToRNA(dna string) (newDNA string) {
+	transcription := map[rune]rune{'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
+
+	for _, nucleotide := range dna {
+		newDNA += string(transcription[nucleotide])
+	}
+
+	return newDNA
 }
