17cb491d59e66305dfb71dccbc465e4d4973836c
Alexandre Barret <alexandre@hatchinvest.nz>
2022-03-19 18:18:50 +1300

    Refactor rna solution

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,11 +1,11 @@
 package strand
 
-func ToRNA(dna string) (newDNA string) {
-	transcription := map[rune]rune{'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
+func ToRNA(dna string) (rna string) {
+	DNAToRNA := map[rune]rune{'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
 
-	for _, nucleotide := range dna {
-		newDNA += string(transcription[nucleotide])
+	for _, n := range dna {
+		rna += string(DNAToRNA[n])
 	}
 
-	return newDNA
+	return rna
 }
