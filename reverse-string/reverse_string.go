c63d6f91114efd436f63e8111b1af765f2f43f3d
Alexandre Barret <alexandre@hatchinvest.nz>
2022-03-23 20:13:01 +1300

    solve reverse-string

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -1,5 +1,11 @@
 package reverse
 
-func Reverse(input string) string {
-	panic("Please implement the Reverse function")
+func Reverse(input string) (result string) {
+	runes, rInputs := []rune{}, []rune(input)
+
+	for r := len(rInputs) - 1; r >= 0; r-- {
+		runes = append(runes, rInputs[r])
+	}
+
+	return string(runes)
 }
