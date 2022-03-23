352a06e48246a4ac911bfceedf4cecd7990aa0b6
Alexandre Barret <alexandre@hatchinvest.nz>
2022-03-23 20:40:28 +1300

    Solve anagram

--- anagram/anagram.go
+++ anagram/anagram.go
@@ -1,5 +1,28 @@
 package anagram
 
+import (
+	"sort"
+	"strings"
+)
+
 func Detect(subject string, candidates []string) []string {
-	panic("Please implement the Detect function")
+	anagrams := map[string][]string{}
+
+	for _, candidate := range candidates {
+		if strings.ToLower(candidate) == strings.ToLower(subject) {
+			continue
+		}
+		anagrams[AnagramForm(candidate)] = append(anagrams[AnagramForm(candidate)], candidate)
+	}
+
+	return anagrams[AnagramForm(subject)]
+}
+
+func AnagramForm(s string) string {
+	runes := []rune(strings.ToLower(s))
+	sort.Slice(runes, func(i, j int) bool {
+		return runes[i] < runes[j]
+	})
+
+	return string(runes)
 }
