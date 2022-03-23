07efd97242859066103a7c8e03bb9d7663b7ccc2
Alexandre Barret <alexandre@hatchinvest.nz>
2022-03-23 19:50:58 +1300

    solve pangram

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,5 +1,21 @@
 package pangram
 
+import (
+	"strings"
+)
+
 func IsPangram(input string) bool {
-	panic("Please implement the IsPangram function")
+	result := map[rune]bool{}
+
+	for _, r := range strings.ToLower(input) {
+		result[r] = true
+	}
+
+	for l := 'a'; l < 'z'; l++ {
+		if !result[l] {
+			return false
+		}
+	}
+
+	return true
 }
