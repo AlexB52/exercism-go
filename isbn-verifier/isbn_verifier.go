b75d8d2a4a19658cad05931937ab00aef8495f87
Alexandre Barret <alexandre@hatchinvest.nz>
2022-03-25 22:11:40 +1300

    solve isbn-verifier

--- isbn-verifier/isbn_verifier.go
+++ isbn-verifier/isbn_verifier.go
@@ -1,5 +1,28 @@
 package isbn
 
+import (
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+	isbn = strings.ReplaceAll(isbn, "-", "")
+	matched, _ := regexp.Match(`\d{9}[\d|X]`, []byte(isbn))
+
+	if len(isbn) != 10 || !matched {
+		return false
+	}
+
+	var checksum int
+	for i := 0; i < 10; i++ {
+		n, err := strconv.Atoi(string(isbn[i]))
+		if err != nil {
+			n = 10
+		}
+
+		checksum += (10 - i) * n
+	}
+
+	return checksum%11 == 0
 }
