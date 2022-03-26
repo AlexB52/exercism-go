5f1659f7f47f0db922be7434a6de963e04b9f0bb
Alexandre Barret <alexandre@hatchinvest.nz>
2022-03-26 13:49:34 +1300

    solve largest-series-product

--- largest-series-product/largest_series_product.go
+++ largest-series-product/largest_series_product.go
@@ -1,5 +1,31 @@
 package lsproduct
 
+import (
+	"errors"
+	"strconv"
+)
+
 func LargestSeriesProduct(digits string, span int) (int64, error) {
-	panic("Please implement the LargestSeriesProduct function")
+	if len(digits) < span || span < 0 {
+		return -1, errors.New("span is bigger than number supplied")
+	}
+
+	result := 0
+	for i := 0; i <= len(digits)-span; i++ {
+		product := 1
+		for _, r := range digits[i : i+span] {
+			n, err := strconv.Atoi(string(r))
+			if err != nil {
+				return -1, errors.New("only numbers are allowed")
+			}
+
+			product *= n
+		}
+
+		if product > result {
+			result = product
+		}
+	}
+
+	return int64(result), nil
 }
