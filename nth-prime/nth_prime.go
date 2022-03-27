adf62a351ae0fc313061aba62559cb8c4acb838e
Alexandre Barret <alexandre@hatchinvest.nz>
2022-03-27 17:15:02 +1300

    Solve nth-prime

--- nth-prime/nth_prime.go
+++ nth-prime/nth_prime.go
@@ -1,5 +1,25 @@
 package prime
 
 func Nth(n int) (int, bool) {
-	panic("Implement the func for it to return Nth prime number")
+	if n == 0 {
+		return 0, false
+	}
+
+	primes := []int{}
+	for i := 2; len(primes) < n; i++ {
+
+		isPrime := true
+		for _, p := range primes {
+			if i%p == 0 {
+				isPrime = false
+				break
+			}
+		}
+
+		if isPrime {
+			primes = append(primes, i)
+		}
+	}
+
+	return primes[len(primes)-1], true
 }
