977878b120a25b51498ac0ebda501f4954814bfe
Alexandre Barret <alexandre@hatchinvest.nz>
2022-03-28 18:34:03 +1300

    solve sum-of-multiples

--- sum-of-multiples/sum_of_multiples.go
+++ sum-of-multiples/sum_of_multiples.go
@@ -1,5 +1,18 @@
 package summultiples
 
-func SumMultiples(limit int, divisors ...int) int {
-	panic("Please implement the SumMultiples function")
+func SumMultiples(limit int, divisors ...int) (result int) {
+	var numbers = map[int]bool{}
+
+	for _, divisor := range divisors {
+		for i := 0; i < limit; i++ {
+			if divisor != 0 && i%divisor == 0 {
+				numbers[i] = true
+			}
+		}
+	}
+
+	for n, _ := range numbers {
+		result += n
+	}
+	return result
 }
