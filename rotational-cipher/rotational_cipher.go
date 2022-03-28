683e2d10928c97271f1578a8896ad5f784bd1b64
Alexandre Barret <alexandre@hatchinvest.nz>
2022-03-28 20:30:40 +1300

    solve rotational-cipher

--- rotational-cipher/rotational_cipher.go
+++ rotational-cipher/rotational_cipher.go
@@ -1,5 +1,26 @@
 package rotationalcipher
 
-func RotationalCipher(plain string, shiftKey int) string {
-	panic("Please implement the RotationalCipher function")
+func RotationalCipher(plain string, shiftKey int) (result string) {
+	for _, r := range plain {
+		var letter rune
+		shiftLetter := rune(int(r) + shiftKey)
+
+		switch {
+		case 'a' <= r && r <= 'z':
+			if shiftLetter > 'z' {
+				shiftLetter -= 26
+			}
+			letter = shiftLetter
+		case 'A' <= r && r <= 'Z':
+			if shiftLetter > 'Z' {
+				shiftLetter -= 26
+			}
+			letter = shiftLetter
+		default:
+			letter = r
+		}
+
+		result += string(letter)
+	}
+	return result
 }
