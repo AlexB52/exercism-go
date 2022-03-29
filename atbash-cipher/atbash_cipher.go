47e6ac803615c603a254b0f54d02b4ee79f5e3ac
Alexandre Barret <alexandre@hatchinvest.nz>
2022-03-29 19:43:44 +1300

    Solve atbash-cypher

--- atbash-cipher/atbash_cipher.go
+++ atbash-cipher/atbash_cipher.go
@@ -1,5 +1,31 @@
 package atbash
 
+import (
+	"strings"
+	"unicode"
+)
+
 func Atbash(s string) string {
-	panic("Please implement the Atbash function")
+	s = strings.ToLower(s)
+	var groups []string
+
+	for i := 0; i < len(s); {
+		var group string
+
+		for i < len(s) && len(group) < 5 {
+			r := rune(s[i])
+			if unicode.IsLetter(r) {
+				group += string(rune('z' + 'a' - r))
+			} else if unicode.IsDigit(r) {
+				group += string(r)
+			}
+			i++
+		}
+
+		if len(group) > 0 {
+			groups = append(groups, group)
+		}
+	}
+
+	return strings.Join(groups, " ")
 }
