1788ea94d84664bafadf43e7f7222823041657ca
Alexandre Barret <alexandre@hatchinvest.nz>
2022-04-05 21:37:07 +1200

    Solve animal-magic

--- animal-magic/animal_magic.go
+++ animal-magic/animal_magic.go
@@ -1,21 +1,36 @@
 package chance
 
+import (
+	"math/rand"
+	"time"
+)
+
 // SeedWithTime seeds math/rand with the current computer time
 func SeedWithTime() {
-	panic("Please implement the SeedWithTime function")
+	rand.Seed(time.Now().UnixNano())
 }
 
 // RollADie returns a random int d with 1 <= d <= 20
 func RollADie() int {
-	panic("Please implement the RollADie function")
+	if n := rand.Intn(20); n != 0 {
+		return n
+	}
+	return RollADie()
 }
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0
 func GenerateWandEnergy() float64 {
-	panic("Please implement the GenerateWandEnergy function")
+	if n := rand.Float64(); n <= 0.12 {
+		return n
+	}
+	return GenerateWandEnergy()
 }
 
 // ShuffleAnimals returns a slice with all eight animal strings in random order
 func ShuffleAnimals() []string {
-	panic("Please implement the ShuffleAnimals function")
+	animals := []string{"ant", "beaver", "cat", "dog", "elephant", "fox", "giraffe", "hedgehog"}
+	rand.Shuffle(len(animals), func(i, j int) {
+		animals[i], animals[j] = animals[j], animals[i]
+	})
+	return animals
 }
