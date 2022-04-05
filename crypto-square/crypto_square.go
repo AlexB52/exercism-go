f9644c5f52760b92663580e43dec70aebaa2299f
Alexandre Barret <alexandre@hatchinvest.nz>
2022-04-05 22:55:39 +1200

    Refactor from community solutions

--- crypto-square/crypto_square.go
+++ crypto-square/crypto_square.go
@@ -9,15 +9,14 @@ import (
 func Encode(pt string) string {
 	re := regexp.MustCompile(`\w`)
 	matches := re.FindAllString(strings.ToLower(pt), -1)
+
 	r, c := MatrixDetails(len(matches))
 
 	var groups = make([]string, c)
 	for i := 0; i < c*r; i++ {
-		var s string
+		s := " "
 		if i < len(matches) {
 			s = matches[i]
-		} else {
-			s = " "
 		}
 
 		groups[i%c] += s
@@ -28,10 +27,9 @@ func Encode(pt string) string {
 
 func MatrixDetails(length int) (r int, c int) {
 	r = int(math.Round(math.Sqrt(float64(length))))
+	c = r + 1
 	if r*r >= length {
 		c = r
-	} else {
-		c = r + 1
 	}
 	return r, c
 }
