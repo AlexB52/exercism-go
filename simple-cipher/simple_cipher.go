8e0fca1fcfa39272adf915ef89c6c6ee850f6872
Alexandre Barret <alexandre@hatchinvest.nz>
2022-04-06 20:45:15 +1200

    Refactor from community solutions

--- simple-cipher/simple_cipher.go
+++ simple-cipher/simple_cipher.go
@@ -6,50 +6,24 @@ import (
 	"unicode"
 )
 
-// Define the shift and vigenere types here.
-// Both types should satisfy the Cipher interface.
-
-type Cipher interface {
-	Encode(string) string
-	Decode(string) string
-	Distance(int) int
-}
-
-type shift struct {
-	Cipher
-	offset int
-}
-
 type vigenere struct {
 	Cipher
 	key string
 }
 
 func NewCaesar() Cipher {
-	return NewShift(3)
+	return NewVigenere("d")
 }
 
 func NewShift(distance int) Cipher {
 	if distance == 0 || distance/26 != 0 {
 		return nil
 	}
-	return shift{offset: distance}
-}
-
-func (c shift) Encode(input string) string {
-	return Code(c, input, encode())
-}
-
-func (c shift) Decode(input string) string {
-	return Code(c, input, decode())
-}
-
-func (c shift) Distance(index int) int {
-	return c.offset
+	return NewVigenere(string(transform('a', distance/distance, distance)))
 }
 
 func NewVigenere(key string) Cipher {
-	re := regexp.MustCompile(`^[a-z]+[b-z]+$`)
+	re := regexp.MustCompile(`^[b-z]$|^[a-z]+[b-z]+$`)
 	if len(key) == 0 || len(re.FindAllString(key, -1)) != 1 {
 		return nil
 	}
@@ -58,44 +32,32 @@ func NewVigenere(key string) Cipher {
 
 func (v vigenere) Encode(input string) string {
 	re := regexp.MustCompile(`\w`)
-	return Code(v, strings.Join(re.FindAllString(input, -1), ""), encode())
+	input = strings.Join(re.FindAllString(input, -1), "")
+	return v.Code(input, 1)
 }
 
 func (v vigenere) Decode(input string) string {
-	return Code(v, input, decode())
+	return v.Code(input, -1)
 }
 
-func (v vigenere) Distance(index int) int {
-	return int(v.key[index%len(v.key)] - 'a')
-}
-
-func Code(c Cipher, input string, transform func(rune, int) rune) string {
+func (v vigenere) Code(input string, sign int) string {
 	var result []rune
 	for i, r := range strings.ToLower(input) {
 		if unicode.IsLetter(r) {
-			result = append(result, transform(r, c.Distance(i)))
+			distance := int(v.key[i%len(v.key)] - 'a')
+			result = append(result, transform(r, sign, distance))
 		}
 	}
 	return string(result)
 }
 
-func decode() func(rune, int) rune {
-	return offset(-1)
-}
-
-func encode() func(rune, int) rune {
-	return offset(1)
-}
-
-func offset(sign int) func(rune, int) rune {
-	return func(r rune, distance int) rune {
-		var utf8Distance = 'z' - 'a' + 1
-		r += rune(sign * distance)
-		if r > 'z' {
-			r -= utf8Distance
-		} else if r < 'a' {
-			r += utf8Distance
-		}
-		return r
+func transform(r rune, sign int, distance int) rune {
+	var utf8Distance = 'z' - 'a' + 1
+	r += rune(sign * distance)
+	if r > 'z' {
+		r -= utf8Distance
+	} else if r < 'a' {
+		r += utf8Distance
 	}
+	return r
 }
