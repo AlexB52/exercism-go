3ea41d0cec06ae9868a38f609ac14ab13baa0aea
Alexandre Barret <alexandre@hatchinvest.nz>
2022-04-07 22:51:48 +1200

    Solve from community solutions
    I couldn't understand what was required from the exercise brief or how to start. I found it too obscure.

--- diffie-hellman/diffie_hellman.go
+++ diffie-hellman/diffie_hellman.go
@@ -1,22 +1,31 @@
 package diffiehellman
 
-import "math/big"
+import (
+	"crypto/rand"
+	"math/big"
+)
 
 // Diffie-Hellman-Merkle key exchange
 // Private keys should be generated randomly.
 
 func PrivateKey(p *big.Int) *big.Int {
-	panic("Please implement the PrivateKey function")
+	delta := big.NewInt(2)
+	n, err := rand.Int(rand.Reader, new(big.Int).Sub(p, delta))
+	if err != nil {
+		panic("Failed to generate private key")
+	}
+	return n.Add(n, delta)
 }
 
 func PublicKey(private, p *big.Int, g int64) *big.Int {
-	panic("Please implement the PublicKey function")
+	return new(big.Int).Exp(big.NewInt(g), private, p)
 }
 
 func NewPair(p *big.Int, g int64) (*big.Int, *big.Int) {
-	panic("Please implement the NewPair function")
+	private := PrivateKey(p)
+	return private, PublicKey(private, p, g)
 }
 
 func SecretKey(private1, public2, p *big.Int) *big.Int {
-	panic("Please implement the SecretKey function")
+	return new(big.Int).Exp(public2, private1, p)
 }
