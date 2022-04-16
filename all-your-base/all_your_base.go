1389f71435935ecb3e88042f1a8b49f7f1c27e79
Alexandre Barret <alexandre@hatchinvest.nz>
2022-04-16 17:02:20 +1200

    solve all-your-base

--- all-your-base/all_your_base.go
+++ all-your-base/all_your_base.go
@@ -1,5 +1,42 @@
 package allyourbase
 
+import (
+	"errors"
+	"math"
+)
+
 func ConvertToBase(inputBase int, inputDigits []int, outputBase int) ([]int, error) {
-	panic("Please implement the ConvertToBase function")
+	if inputBase <= 1 {
+		return nil, errors.New("input base must be >= 2")
+	}
+
+	if outputBase <= 1 {
+		return nil, errors.New("output base must be >= 2")
+	}
+
+	for _, d := range inputDigits {
+		if d < 0 || d >= inputBase {
+			return nil, errors.New("all digits must satisfy 0 <= d < input base")
+		}
+	}
+
+	var number int
+	for i := 0; i < len(inputDigits); i++ {
+		d := float64(inputDigits[i])
+		e := float64(len(inputDigits) - 1 - i)
+		b := float64(inputBase)
+		number += int(d * math.Pow(b, e))
+	}
+
+	if number == 0 {
+		return []int{0}, nil
+	}
+
+	var result []int
+	for number > 0 {
+		r := number % outputBase
+		result = append([]int{r}, result...)
+		number = number / outputBase
+	}
+	return result, nil
 }
