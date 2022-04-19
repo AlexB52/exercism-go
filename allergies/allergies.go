4e63e6f0b983bdafd342d340ba5b68bf9936aabb
Alexandre Barret <alexandre@hatchinvest.nz>
2022-04-19 18:01:14 +1200

    Solve allergies

--- allergies/allergies.go
+++ allergies/allergies.go
@@ -1,9 +1,30 @@
 package allergies
 
+import (
+	"math"
+)
+
+var ALLERGIES = []string{"eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"}
+
 func Allergies(allergies uint) []string {
-	panic("Please implement the Allergies function")
+	allergies %= 256
+
+	var out []string
+	for i := 7; i >= 0; i-- {
+		d := uint(math.Pow(2, float64(i)))
+		if allergies/d == 1 {
+			out = append(out, ALLERGIES[i])
+		}
+		allergies %= d
+	}
+	return out
 }
 
 func AllergicTo(allergies uint, allergen string) bool {
-	panic("Please implement the AllergicTo function")
+	for _, a := range Allergies(allergies) {
+		if a == allergen {
+			return true
+		}
+	}
+	return false
 }
