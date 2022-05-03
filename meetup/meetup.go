d214b7c4916af4f0df10b6c07b3735fca47c97fb
Alexandre Barret <alexandre@hatchinvest.nz>
2022-05-03 18:00:47 +1200

    refactor meetup

--- meetup/meetup.go
+++ meetup/meetup.go
@@ -17,37 +17,31 @@ const (
 )
 
 func Day(week WeekSchedule, weekday time.Weekday, month time.Month, year int) int {
-	t := time.Date(year, month+1, 0, 0, 0, 0, 0, time.UTC)
-
-	for day := 1; day <= t.Day(); day++ {
-		date := time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
-
-		switch week {
-		case First:
-			if weekday == date.Weekday() && day > 0 {
-				return day
-			}
-		case Second:
-			if weekday == date.Weekday() && day > 7 {
-				return day
-			}
-		case Third:
-			if weekday == date.Weekday() && day > 14 {
-				return day
-			}
-		case Fourth:
-			if weekday == date.Weekday() && day > 21 {
-				return day
-			}
-		case Teenth:
-			if weekday == date.Weekday() && day >= 13 && day <= 19 {
-				return day
-			}
-		case Last:
-			if weekday == date.Weekday() && day > t.Day()-7 {
-				return day
-			}
+	start, end := DayRange(week, year, month)
+	for day := start; day <= end; day++ {
+		if weekday == time.Date(year, month, day, 0, 0, 0, 0, time.UTC).Weekday() {
+			return day
 		}
 	}
 	return 0
+}
+
+func DayRange(week WeekSchedule, year int, month time.Month) (start int, end int) {
+	switch week {
+	case First:
+		return 1, 8
+	case Second:
+		return 8, 15
+	case Third:
+		return 15, 22
+	case Fourth:
+		return 22, 29
+	case Teenth:
+		return 13, 19
+	case Last:
+		lastDay := time.Date(year, month+1, 0, 0, 0, 0, 0, time.UTC).Day()
+		return lastDay - 6, lastDay
+	default:
+		return 0, 0
+	}
 }
