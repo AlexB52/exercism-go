3b1d6a14fa3f2bfaefe3b27581e6f2756b6d6800
Alexandre Barret <alexandre@hatchinvest.nz>
2022-05-07 09:43:36 +1200

    Solve prime-factors

--- prime-factors/prime_factors.go
+++ prime-factors/prime_factors.go
@@ -1,5 +1,20 @@
 package prime
 
-func Factors(n int64) []int64 {
-	panic("Please implement the Factors function")
+func Factors(n int64) (factors []int64) {
+	if n <= 1 {
+		return nil
+	}
+
+prime:
+	for n >= 2 {
+		for i := int64(2); i <= n; i++ {
+			if n%i == 0 {
+				factors = append(factors, i)
+				n = n / i
+				continue prime
+			}
+		}
+	}
+
+	return factors
 }
