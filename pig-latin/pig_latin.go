e3ea2010e382f5f4cec3b783450ba2d36e60d089
Alexandre Barret <alexandre@hatchinvest.nz>
2022-05-10 21:17:34 +1200

    Solve pig-latin

--- pig-latin/pig_latin.go
+++ pig-latin/pig_latin.go
@@ -6,32 +6,35 @@ import (
 	"strings"
 )
 
+var PREFIXES = []string{"ch", "qu", "squ", "thr", "sch", "th", "rh", "f", "m", "r", "p", "k", "x", "q", "c", "h", "y"}
+var EXCEPTIONS = []string{"xr", "yt"}
+
 func Sentence(sentence string) string {
 	var result []string
 	re := regexp.MustCompile("\\w+")
 
 	for _, w := range re.FindAllString(sentence, -1) {
-		result = append(result, Convert(w))
+		result = append(result, PigLatin(w))
 	}
 
 	return strings.Join(result, " ")
 }
 
-func Convert(word string) string {
-	consonants := []string{"ch", "qu", "squ", "thr", "sch", "th", "rh", "f", "m", "r", "p", "k", "x", "q", "c", "h", "y"}
-	exceptions := []string{"xr", "yt"}
-
-swap:
-	for _, c := range consonants {
-		for _, e := range exceptions {
-			if strings.HasPrefix(word, e) {
-				break swap
-			}
-		}
-
-		if strings.HasPrefix(word, c) {
-			word = fmt.Sprintf("%s%s", strings.TrimPrefix(word, c), c)
+func PigLatin(word string) string {
+	swapPrefix := true
+	for _, e := range EXCEPTIONS {
+		if strings.HasPrefix(word, e) {
+			swapPrefix = false
 			break
+		}
+	}
+
+	if swapPrefix {
+		for _, c := range PREFIXES {
+			if strings.HasPrefix(word, c) {
+				word = fmt.Sprintf("%s%s", strings.TrimPrefix(word, c), c)
+				break
+			}
 		}
 	}
 
