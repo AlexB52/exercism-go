5a1d83822b7928b491b3060deb8936f2bf212705
Alexandre Barret <alexandre@hatchinvest.nz>
2022-05-10 21:05:23 +1200

    Solve pig-latin

--- pig-latin/pig_latin.go
+++ pig-latin/pig_latin.go
@@ -1,5 +1,39 @@
 package piglatin
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
 func Sentence(sentence string) string {
-	panic("Please implement the Sentence function")
+	var result []string
+	re := regexp.MustCompile("\\w+")
+
+	for _, w := range re.FindAllString(sentence, -1) {
+		result = append(result, Convert(w))
+	}
+
+	return strings.Join(result, " ")
+}
+
+func Convert(word string) string {
+	consonants := []string{"ch", "qu", "squ", "thr", "sch", "th", "rh", "f", "m", "r", "p", "k", "x", "q", "c", "h", "y"}
+	exceptions := []string{"xr", "yt"}
+
+swap:
+	for _, c := range consonants {
+		for _, e := range exceptions {
+			if strings.HasPrefix(word, e) {
+				break swap
+			}
+		}
+
+		if strings.HasPrefix(word, c) {
+			word = fmt.Sprintf("%s%s", strings.TrimPrefix(word, c), c)
+			break
+		}
+	}
+
+	return fmt.Sprintf("%say", word)
 }
