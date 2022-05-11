7ff4270ae73c6e51a46a4b8dc476d4a3e5a34875
Alexandre Barret <alexandre@hatchinvest.nz>
2022-05-11 22:31:25 +1200

    yeah

--- ledger/ledger.go
+++ ledger/ledger.go
@@ -126,42 +126,22 @@ func FormatUSRow(currency string, entry Entry) (string, error) {
 }
 
 func FormatRow(locale, currency string, entry Entry) (string, error) {
-	t, err := time.Parse("2006-02-01", entry.Date)
-	if err != nil {
-		return "", errors.New("")
-	}
+	var result string
+	var err error
 
-	var description string
-	if len(entry.Description) > 25 {
-		description = fmt.Sprintf("%-22.22s...", entry.Description)
-	} else {
-		description = fmt.Sprintf("%-25s", entry.Description)
-	}
-
-	var symbol string
-	if currency == "EUR" {
-		symbol = "€"
-	} else {
-		symbol = "$"
-	}
-
-	var a, date string
 	if locale == "nl-NL" {
-		result, err := FormatDutchRow(currency, entry)
+		result, err = FormatDutchRow(currency, entry)
 		if err != nil {
 			return "", err
 		}
 		return result, nil
 	} else if locale == "en-US" {
-		date = t.Format("02/01/2006")
-		if entry.Change < 0 {
-			a = fmt.Sprintf("(%s%s)", symbol, FormatChange(entry.Change, ",", "."))
-		} else {
-			a = fmt.Sprintf(" %s%s ", symbol, FormatChange(entry.Change, ",", "."))
+		result, err = FormatUSRow(currency, entry)
+		if err != nil {
+			return "", err
 		}
 	}
-
-	return fmt.Sprintf("%10s | %s | %13s\n", date, description, a), nil
+	return result, err
 }
 
 func FormatChange(change int, tsep, csep string) (result string) {
