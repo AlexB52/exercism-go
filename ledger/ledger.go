0f8c76594cda7cb74c4f20b3027e4266b407cee4
Alexandre Barret <alexandre@hatchinvest.nz>
2022-05-11 20:10:24 +1200

    yeah

--- ledger/ledger.go
+++ ledger/ledger.go
@@ -74,7 +74,7 @@ func FormatLedger(currency string, locale string, entries []Entry) (string, erro
 			if len(de) > 25 {
 				de = de[:22] + "..."
 			} else {
-				de = de + strings.Repeat(" ", 25-len(de))
+				de += strings.Repeat(" ", 25-len(de))
 			}
 			var d string
 			if locale == "nl-NL" {
