51993e6b74c908acaf2883357c3d11c6083ba8d1
Alexandre Barret <alexandre@hatchinvest.nz>
2022-05-23 19:13:37 +1200

    yeah

--- ledger/ledger.go
+++ ledger/ledger.go
@@ -118,22 +118,6 @@ func BuildRow(currency string) func(e Entry, currency string) (Row, error) {
 	}
 }
 
-func BuildUSRow(e Entry, currency string) (Row, error) {
-	date, err := time.Parse("2006-02-01", e.Date)
-	if err != nil {
-		return Row{}, errors.New("")
-	}
-
-	var change string
-	if e.Change < 0 {
-		change = fmt.Sprintf("(%s%s)", Symbol(currency), FormatChange(e.Change, ",", "."))
-	} else {
-		change = fmt.Sprintf(" %s%s ", Symbol(currency), FormatChange(e.Change, ",", "."))
-	}
-
-	return Row{date.Format("02/01/2006"), FormatDescription(e.Description), change}, nil
-}
-
 func Symbol(currency string) string {
 	if currency == "EUR" {
 		return "€"
