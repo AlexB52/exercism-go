3d1de83947ecf914c88259794d4fc06d013902c9
Alexandre Barret <alexandre@hatchinvest.nz>
2022-05-23 17:50:23 +1200

    yeah

--- ledger/ledger.go
+++ ledger/ledger.go
@@ -81,6 +81,19 @@ func FormatDutchRows(entries []Entry, currency string) (rows []string, err error
 	return rows, nil
 }
 
+func FormatDutchRows2(entries []Entry, currency string) (result string, err error) {
+	// rows = append(rows, fmt.Sprintf("%-10s | %-25s | %s\n", "Datum", "Omschrijving", "Verandering"))
+	// for _, e := range entries {
+	// 	row, err := FormatDutchRow(currency, e)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// 	rows = append(rows, row)
+	// }
+
+	return "", nil
+}
+
 func FormatUSRows(entries []Entry, currency string) (result string, err error) {
 	var rows []string
 	rows = append(rows, fmt.Sprintf("%-10s | %-25s | %s\n", "Date", "Description", "Change"))
