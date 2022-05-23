a646c4db52904f28ebd42fa7d9dc707227e0561c
Alexandre Barret <alexandre@hatchinvest.nz>
2022-05-23 19:42:58 +1200

    yeah

--- ledger/ledger.go
+++ ledger/ledger.go
@@ -48,8 +48,8 @@ func FormatLedger(currency string, locale string, entries []Entry) (table string
 		buildRow = BuildRow("01-02-2006", FormatDutchChange(CURRENCY_SYMBOL[currency]))
 	case "en-US":
 		header = Row{"Date", "Description", "Change"}
-		buildRow = BuildRow("02/01/2006", FormatUSChange(CURRENCY_SYMBOL[currency]))
 		buildRow = BuildUSRow(CURRENCY_SYMBOL[currency])
+		buildRow = BuildRow("02/01/2006", FormatUSChange(CURRENCY_SYMBOL[currency]))
 	}
 
 	table, err = BuildTable(header, buildRow, entriesCopy)
