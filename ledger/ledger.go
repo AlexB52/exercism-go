ae765f8d901b21f8cc953ed573e0d4a224f85dc7
Alexandre Barret <alexandre@hatchinvest.nz>
2022-05-23 18:41:44 +1200

    yeah

--- ledger/ledger.go
+++ ledger/ledger.go
@@ -171,6 +171,14 @@ func Description(entry Entry) string {
 	}
 }
 
+func FormatDescription(s string) string {
+	if len(s) > 25 {
+		return fmt.Sprintf("%-22.22s...", s)
+	} else {
+		return fmt.Sprintf("%-25s", s)
+	}
+}
+
 func FormatChange(change int, tsep, csep string) (result string) {
 	if change < 0 {
 		change *= -1
