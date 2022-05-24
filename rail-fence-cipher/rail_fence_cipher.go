cc89ce1aeb860d01e2a4c24a9e9a6447af8b50b6
Alexandre Barret <alexandre@hatchinvest.nz>
2022-05-24 20:14:39 +1200

    solve rails fence cipher

--- rail-fence-cipher/rail_fence_cipher.go
+++ rail-fence-cipher/rail_fence_cipher.go
@@ -1,9 +1,74 @@
 package railfence
 
-func Encode(message string, rails int) string {
-	panic("Please implement the Encode function")
+import (
+	"sort"
+)
+
+type Node struct {
+	x, y  int
+	value string
 }
 
-func Decode(message string, rails int) string {
-	panic("Please implement the Decode function")
+func DiagonaleProjection(rail int) func(int) Node {
+	return func(pos int) Node {
+		x, y := pos, pos%(2*(rail-1))
+		if y >= rail {
+			y = rail - (y % rail) - 2
+		}
+		return Node{x: x, y: y}
+	}
+}
+
+func Encode(message string, rails int) (result string) {
+	var nodes []Node
+	projection := DiagonaleProjection(rails)
+	for i, r := range message {
+		n := projection(i)
+		n.value = string(r)
+		nodes = append(nodes, n)
+	}
+
+	sort.Slice(nodes, func(i, j int) bool {
+		if nodes[i].y < nodes[j].y {
+			return true
+		} else if nodes[i].y > nodes[j].y {
+			return false
+		}
+		return nodes[i].x < nodes[j].x
+	})
+
+	for _, n := range nodes {
+		result += n.value
+	}
+	return result
+}
+
+func Decode(message string, rails int) (result string) {
+	var nodes = make([]Node, len(message))
+	projection := DiagonaleProjection(rails)
+	for i := 0; i < len(message); i++ {
+		nodes[i] = projection(i)
+	}
+
+	sort.Slice(nodes, func(i, j int) bool {
+		if nodes[i].y < nodes[j].y {
+			return true
+		} else if nodes[i].y > nodes[j].y {
+			return false
+		}
+		return nodes[i].x < nodes[j].x
+	})
+
+	for i, n := range nodes {
+		nodes[i] = Node{n.x, n.y, string(message[i])}
+	}
+
+	sort.Slice(nodes, func(i, j int) bool {
+		return nodes[i].x < nodes[j].x
+	})
+
+	for _, n := range nodes {
+		result += n.value
+	}
+	return result
 }
--- rail-fence-cipher/rail_fence_cipher_test.go
+++ rail-fence-cipher/rail_fence_cipher_test.go
@@ -1,6 +1,9 @@
 package railfence
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func testCases(op func(string, int) string, cases []testCase, t *testing.T) {
 	for _, tc := range cases {
@@ -13,3 +16,117 @@ func testCases(op func(string, int) string, cases []testCase, t *testing.T) {
 
 func TestEncode(t *testing.T) { testCases(Encode, encodeTests, t) }
 func TestDecode(t *testing.T) { testCases(Decode, decodeTests, t) }
+
+func TestDiagonaleProjection(t *testing.T) {
+	f2Tests := []struct {
+		position int
+		expected Node
+	}{
+		{0, Node{x: 0, y: 0}},
+		{1, Node{x: 1, y: 1}},
+		{2, Node{x: 2, y: 0}},
+		{3, Node{x: 3, y: 1}},
+		{4, Node{x: 4, y: 0}},
+		{5, Node{x: 5, y: 1}},
+		{6, Node{x: 6, y: 0}},
+		{7, Node{x: 7, y: 1}},
+		{8, Node{x: 8, y: 0}},
+		{9, Node{x: 9, y: 1}},
+		{10, Node{x: 10, y: 0}},
+		{11, Node{x: 11, y: 1}},
+	}
+
+	f2 := DiagonaleProjection(2)
+	for _, test := range f2Tests {
+		t.Run(fmt.Sprintf("f2 with position: %d", test.position), func(t *testing.T) {
+			actual := f2(test.position)
+			if actual != test.expected {
+				t.Fatalf("FAIL - Expected: %v Actual: %v", test.expected, actual)
+			}
+		})
+	}
+
+	f3Tests := []struct {
+		position int
+		expected Node
+	}{
+		{0, Node{x: 0, y: 0}},
+		{1, Node{x: 1, y: 1}},
+		{2, Node{x: 2, y: 2}},
+		{3, Node{x: 3, y: 1}},
+		{4, Node{x: 4, y: 0}},
+		{5, Node{x: 5, y: 1}},
+		{6, Node{x: 6, y: 2}},
+		{7, Node{x: 7, y: 1}},
+		{8, Node{x: 8, y: 0}},
+		{9, Node{x: 9, y: 1}},
+		{10, Node{x: 10, y: 2}},
+		{11, Node{x: 11, y: 1}},
+	}
+
+	f3 := DiagonaleProjection(3)
+	for _, test := range f3Tests {
+		t.Run(fmt.Sprintf("f3 with position: %d", test.position), func(t *testing.T) {
+			actual := f3(test.position)
+			if actual != test.expected {
+				t.Fatalf("FAIL - Expected: %v Actual: %v", test.expected, actual)
+			}
+		})
+	}
+
+	f4Tests := []struct {
+		position int
+		expected Node
+	}{
+		{0, Node{x: 0, y: 0}},
+		{1, Node{x: 1, y: 1}},
+		{2, Node{x: 2, y: 2}},
+		{3, Node{x: 3, y: 3}},
+		{4, Node{x: 4, y: 2}},
+		{5, Node{x: 5, y: 1}},
+		{6, Node{x: 6, y: 0}},
+		{7, Node{x: 7, y: 1}},
+		{8, Node{x: 8, y: 2}},
+		{9, Node{x: 9, y: 3}},
+		{10, Node{x: 10, y: 2}},
+		{11, Node{x: 11, y: 1}},
+	}
+
+	f4 := DiagonaleProjection(4)
+	for _, test := range f4Tests {
+		t.Run(fmt.Sprintf("f4 with position: %d", test.position), func(t *testing.T) {
+			actual := f4(test.position)
+			if actual != test.expected {
+				t.Fatalf("FAIL - Expected: %v Actual: %v", test.expected, actual)
+			}
+		})
+	}
+
+	f5Tests := []struct {
+		position int
+		expected Node
+	}{
+		{0, Node{x: 0, y: 0}},
+		{1, Node{x: 1, y: 1}},
+		{2, Node{x: 2, y: 2}},
+		{3, Node{x: 3, y: 3}},
+		{4, Node{x: 4, y: 4}},
+		{5, Node{x: 5, y: 3}},
+		{6, Node{x: 6, y: 2}},
+		{7, Node{x: 7, y: 1}},
+		{8, Node{x: 8, y: 0}},
+		{9, Node{x: 9, y: 1}},
+		{10, Node{x: 10, y: 2}},
+		{11, Node{x: 11, y: 3}},
+	}
+
+	f5 := DiagonaleProjection(5)
+	for _, test := range f5Tests {
+		t.Run(fmt.Sprintf("f5 with position: %d", test.position), func(t *testing.T) {
+			actual := f5(test.position)
+			if actual != test.expected {
+				t.Fatalf("FAIL - Expected: %v Actual: %v", test.expected, actual)
+			}
+		})
+	}
+}
