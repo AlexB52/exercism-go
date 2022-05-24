6edc2d765469a42201fb6249555eb6732b5653ce
Alexandre Barret <alexandre@hatchinvest.nz>
2022-05-24 20:42:54 +1200

    refactor rail fence cipher

--- rail-fence-cipher/rail_fence_cipher.go
+++ rail-fence-cipher/rail_fence_cipher.go
@@ -9,6 +9,31 @@ type Node struct {
 	value string
 }
 
+func Encode(message string, rails int) (result string) {
+	return Code(message, rails, DiagonalSorting, RailsSorting)
+}
+
+func Decode(message string, rails int) (result string) {
+	return Code(message, rails, RailsSorting, DiagonalSorting)
+}
+
+func Code(message string, rails int, sort1, sort2 func([]Node) func(int, int) bool) (result string) {
+	var nodes = BuildNodes(len(message), rails)
+
+	sort.Slice(nodes, sort1(nodes))
+
+	for i, n := range nodes {
+		nodes[i] = Node{n.x, n.y, string(message[i])}
+	}
+
+	sort.Slice(nodes, sort2(nodes))
+
+	for _, n := range nodes {
+		result += n.value
+	}
+	return result
+}
+
 func DiagonaleProjection(rail int) func(int) Node {
 	return func(pos int) Node {
 		x, y := pos, pos%(2*(rail-1))
@@ -19,56 +44,28 @@ func DiagonaleProjection(rail int) func(int) Node {
 	}
 }
 
-func Encode(message string, rails int) (result string) {
-	var nodes []Node
+func BuildNodes(length, rails int) []Node {
+	var nodes = make([]Node, length)
 	projection := DiagonaleProjection(rails)
-	for i, r := range message {
-		n := projection(i)
-		n.value = string(r)
-		nodes = append(nodes, n)
-	}
-
-	sort.Slice(nodes, func(i, j int) bool {
-		if nodes[i].y < nodes[j].y {
-			return true
-		} else if nodes[i].y > nodes[j].y {
-			return false
-		}
-		return nodes[i].x < nodes[j].x
-	})
-
-	for _, n := range nodes {
-		result += n.value
-	}
-	return result
-}
-
-func Decode(message string, rails int) (result string) {
-	var nodes = make([]Node, len(message))
-	projection := DiagonaleProjection(rails)
-	for i := 0; i < len(message); i++ {
+	for i := 0; i < length; i++ {
 		nodes[i] = projection(i)
 	}
+	return nodes
+}
 
-	sort.Slice(nodes, func(i, j int) bool {
+func RailsSorting(nodes []Node) func(int, int) bool {
+	return func(i, j int) bool {
 		if nodes[i].y < nodes[j].y {
 			return true
 		} else if nodes[i].y > nodes[j].y {
 			return false
 		}
 		return nodes[i].x < nodes[j].x
-	})
-
-	for i, n := range nodes {
-		nodes[i] = Node{n.x, n.y, string(message[i])}
 	}
+}
 
-	sort.Slice(nodes, func(i, j int) bool {
+func DiagonalSorting(nodes []Node) func(int, int) bool {
+	return func(i, j int) bool {
 		return nodes[i].x < nodes[j].x
-	})
-
-	for _, n := range nodes {
-		result += n.value
 	}
-	return result
 }
