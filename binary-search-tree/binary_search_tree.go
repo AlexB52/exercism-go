f8df478a0b83fb8ef69b25beedbdeded658c387d
Alexandre Barret <alexandre@hatchinvest.nz>
2022-05-28 18:16:50 +1200

    solve binary-search-tree

--- binary-search-tree/binary_search_tree.go
+++ binary-search-tree/binary_search_tree.go
@@ -1,26 +1,43 @@
 package binarysearchtree
 
+// import "fmt"
+
 type BinarySearchTree struct {
 	left  *BinarySearchTree
 	data  int
 	right *BinarySearchTree
 }
 
-// NewBst creates and returns a new BinarySearchTree.
 func NewBst(i int) *BinarySearchTree {
-	panic("Please implement the NewBst function")
+	return &BinarySearchTree{data: i}
 }
 
-// Insert inserts an int into the BinarySearchTree.
-// Inserts happen based on the rules of a binary search tree
 func (bst *BinarySearchTree) Insert(i int) {
-	panic("Please implement the Insert method")
+	if i > bst.data {
+		if bst.right != nil {
+			bst.right.Insert(i)
+		} else {
+			bst.right = &BinarySearchTree{left: bst.right, data: i}
+		}
+	} else {
+		if bst.left != nil {
+			bst.left.Insert(i)
+		} else {
+			bst.left = &BinarySearchTree{right: bst.left, data: i}
+		}
+	}
 }
 
-// SortedData returns the ordered contents of BinarySearchTree as an []int.
-// The values are in increasing order starting with the lowest int value.
-// A BinarySearchTree that has the numbers [1,3,7,5] added will return the
-// []int [1,3,5,7].
-func (bst *BinarySearchTree) SortedData() []int {
-	panic("Please implement the SortedData function")
+func (bst *BinarySearchTree) SortedData() (result []int) {
+	if bst.left != nil {
+		result = append(result, bst.left.SortedData()...)
+	}
+
+	result = append(result, bst.data)
+
+	if bst.right != nil {
+		result = append(result, bst.right.SortedData()...)
+	}
+
+	return result
 }
