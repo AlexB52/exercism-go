059c37bd4f26e0bd1b68b1f9dfc17f55a1a8f327
Alexandre Barret <alexandre@hatchinvest.nz>
2022-05-28 13:26:55 +1200

    Solve binary-search

--- binary-search/binary_search.go
+++ binary-search/binary_search.go
@@ -1,5 +1,22 @@
 package binarysearch
 
 func SearchInts(list []int, key int) int {
-	panic("Please implement the SearchInts function")
+	var index int
+	var start, end = 0, len(list)
+	for len(list[start:end]) > 1 {
+		index = (end + start) / 2
+		if list[index] > key {
+			end = index
+		} else if list[index] < key {
+			start = index
+		} else if list[index] == key {
+			return index
+		}
+	}
+
+	if len(list[start:end]) == 1 && list[start:end][0] == key {
+		return start
+	}
+
+	return -1
 }
