ed64f58a70835ab8c6d9e61f1b7e71690ce78669
Alexandre Barret <alexandre@hatchinvest.nz>
2022-05-30 19:50:31 +1200

    Solve brackets

--- matching-brackets/matching_brackets.go
+++ matching-brackets/matching_brackets.go
@@ -1,5 +1,27 @@
 package brackets
 
+var PAIRS = map[rune]rune{
+	'}': '{',
+	']': '[',
+	')': '(',
+}
+
 func Bracket(input string) bool {
-	panic("Please implement the Bracket function")
+	var inputs []rune
+	for i := 0; i < len(input); i++ {
+		current := rune(input[i])
+		switch current {
+		case '[', '{', '(':
+			inputs = append(inputs, current)
+		case ']', '}', ')':
+			if len(inputs) > 0 && inputs[len(inputs)-1] == PAIRS[current] {
+				inputs = inputs[:len(inputs)-1]
+			} else {
+				return false
+			}
+		default:
+		}
+	}
+
+	return len(inputs) == 0
 }
