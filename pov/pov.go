c70cd07e62e2de7528e7b2bde40314cd3b972b0a
Alexandre Barret <alexandre@hatchinvest.nz>
2022-06-04 11:57:54 +1200

    Solve FromPov

--- pov/pov.go
+++ pov/pov.go
@@ -1,24 +1,30 @@
 package pov
 
 type Tree struct {
-	// Add the needed fields here
+	value    string
+	children []*Tree
+	parent   *Tree
 }
 
 // New creates and returns a new Tree with the given root value and children.
 func New(value string, children ...*Tree) *Tree {
-	panic("Please implement this function")
+	tree := &Tree{value: value, children: children}
+	for _, trc := range tree.Children() {
+		trc.parent = tree
+	}
+	return tree
 }
 
 // Value returns the value at the root of a tree.
 func (tr *Tree) Value() string {
-	panic("Please implement this function")
+	return tr.value
 }
 
-// Children returns a slice containing the children of a tree.
+// Children returns a slice ßcontaining the children of a tree.
 // There is no need to sort the elements in the result slice,
 // they can be in any order.
 func (tr *Tree) Children() []*Tree {
-	panic("Please implement this function")
+	return tr.children
 }
 
 // String describes a tree in a compact S-expression format.
@@ -42,10 +48,44 @@ func (tr *Tree) String() string {
 
 // FromPov returns the pov from the node specified in the argument.
 func (tr *Tree) FromPov(from string) *Tree {
-	panic("Please implement this function")
+	var refTree = map[string]*Tree{}
+	BuildRef(refTree, tr)
+
+	return BuildTree(refTree, &Tree{value: from})
+}
+
+func BuildRef(m map[string]*Tree, t *Tree) {
+	m[t.Value()] = t
+	for _, n := range t.Children() {
+		m[n.Value()] = n
+		BuildRef(m, n)
+	}
+}
+
+func BuildTree(ref map[string]*Tree, node *Tree) *Tree {
+	tree, ok := ref[node.Value()]
+	if !ok {
+		return nil
+	}
+
+	children := tree.Children()
+	if tree.parent != nil {
+		children = append(children, tree.parent)
+	}
+
+	for _, n := range children {
+		if node.parent != nil && n.Value() == node.parent.Value() {
+			continue
+		}
+		child := &Tree{n.Value(), nil, node}
+		node.children = append(node.children, child)
+		BuildTree(ref, child)
+	}
+
+	return node
 }
 
 // PathTo returns the shortest path between two nodes in the tree.
 func (tr *Tree) PathTo(from, to string) []string {
-	panic("Please implement this function")
+	return nil
 }
