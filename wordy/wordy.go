a76aa09a3f23c78c82a08b5e43aac4e49f1ad35a
Alexandre Barret <alexandre@hatchinvest.nz>
2022-06-04 22:06:33 +1200

    solve wordy

--- wordy/wordy.go
+++ wordy/wordy.go
@@ -1,5 +1,39 @@
 package wordy
 
+import (
+	"regexp"
+	"strconv"
+)
+
+var (
+	VALIDATION_REGEX = `What is (-?\d+)+( (plus|minus|multiplied by|divided by) (-?\d+))*\?`
+	OPERATIONS_REGEX = `(-?\d+|plus|minus|multiplied by|divided by)`
+)
+
 func Answer(question string) (int, bool) {
-	panic("Please implement the Answer function")
+	if regexp.MustCompile(VALIDATION_REGEX).FindString(question) == "" {
+		return 0, false
+	}
+
+	words := regexp.MustCompile(OPERATIONS_REGEX).FindAllString(question, -1)
+
+	result, _ := strconv.Atoi(words[0])
+
+	for i := 1; i < len(words); i += 2 {
+		n, _ := strconv.Atoi(words[i+1])
+		switch words[i] {
+		case "plus":
+			result += n
+		case "minus":
+			result -= n
+		case "multiplied by":
+			result *= n
+		case "divided by":
+			result /= n
+		default:
+			return 0, false
+		}
+	}
+
+	return result, true
 }
