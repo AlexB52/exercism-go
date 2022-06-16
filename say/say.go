0f744d11172c1bebe9993f30e91f003a8659df87
Alexandre Barret <alexandre@hatchinvest.nz>
2022-06-16 22:23:41 +1200

    Refactor say

--- say/say.go
+++ say/say.go
@@ -10,103 +10,35 @@ func Say(n int64) (string, bool) {
 		return "", false
 	}
 
+	if n == 0 {
+		return "zero", true
+	}
+
 	var result []string
-	switch {
-	case n < 100:
-		result = append(result, Hundreds(n))
-	case n < 1000:
-		if n/1000 > 0 {
-			result = append(result, fmt.Sprintf("%s thousand", Hundreds(n/1000)))
-			n = n % 1000
-		}
-
-		if n/100 > 0 {
-			result = append(result, fmt.Sprintf("%s hundred", Hundreds(n/100)))
-			n = n % 100
-		}
-
-		if n/1 > 0 {
-			result = append(result, Hundreds(n/1))
-		}
-	case n < 1_000_000:
-		if n/1_000_000 > 0 {
-			result = append(result, fmt.Sprintf("%s million", Hundreds(n/1_000_000)))
-			n = n % 1_000_000
-		}
-
-		if n/1000 > 0 {
-			result = append(result, fmt.Sprintf("%s thousand", Hundreds(n/1000)))
-			n = n % 1000
-		}
-
-		if n/100 > 0 {
-			result = append(result, fmt.Sprintf("%s hundred", Hundreds(n/100)))
-			n = n % 100
-		}
-
-		if n/1 > 0 {
-			result = append(result, Hundreds(n/1))
-		}
-	case n < 1_000_000_000:
-		if n/1_000_000 > 0 {
-			result = append(result, fmt.Sprintf("%s million", Hundreds(n/1_000_000)))
-			n = n % 1_000_000
-		}
-
-		if n/1000 > 0 {
-			result = append(result, fmt.Sprintf("%s thousand", Hundreds(n/1000)))
-			n = n % 1000
-		}
-
-		if n/100 > 0 {
-			result = append(result, fmt.Sprintf("%s hundred", Hundreds(n/100)))
-			n = n % 100
-		}
-
-		if n/1 > 0 {
-			result = append(result, Hundreds(n/1))
-		}
-	case n < 1_000_000_000_000:
-		if n/1_000_000_000 > 0 {
-			result = append(result, fmt.Sprintf("%s billion", Hundreds(n/1_000_000_000)))
-			n = n % 1_000_000_000
-		}
-
-		if n/1_000_000 > 0 {
-			result = append(result, fmt.Sprintf("%s million", Hundreds(n/1_000_000)))
-			n = n % 1_000_000
-		}
-
-		if n/1000 > 0 {
-			result = append(result, fmt.Sprintf("%s thousand", Hundreds(n/1000)))
-			n = n % 1000
-		}
-
-		if n/100 > 0 {
-			result = append(result, fmt.Sprintf("%s hundred", Hundreds(n/100)))
-			n = n % 100
-		}
-
-		if n/1 > 0 {
-			result = append(result, Hundreds(n/1))
+	var i int64
+	for i = 1_000_000_000; i > 100; i /= 1000 {
+		if n/i > 0 {
+			result = append(result, fmt.Sprintf("%s %s", Hundreds(n/i), NUMBERS[i]))
+			n = n % i
 		}
 	}
+
+	if n/1 > 0 {
+		result = append(result, fmt.Sprintf("%s", Hundreds(n/1)))
+	}
+
 	return strings.Join(result, " "), true
 }
 
 func Hundreds(n int64) string {
-	if n == 0 {
-		return NUMBERS[n]
-	}
-
 	var result []string
 	if n/100 > 0 {
-		result = append(result, fmt.Sprintf("%s hundred", NUMBERS[n/100]))
+		result = append(result, fmt.Sprintf("%s %s", NUMBERS[n/100], NUMBERS[100]))
 		n = n % 100
 	}
 
 	if n/1 > 0 {
-		result = append(result, NUMBERS[n/1])
+		result = append(result, fmt.Sprintf("%s", NUMBERS[n/1]))
 	}
 
 	return strings.Join(result, " ")
