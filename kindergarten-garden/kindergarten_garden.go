9526609b41f2f46a7828e67d9722c37357c77d48
Alexandre Barret <alexandre@hatchinvest.nz>
2022-06-20 10:05:22 +1200

    Solve kindergarten-garden

--- kindergarten-garden/kindergarten_garden.go
+++ kindergarten-garden/kindergarten_garden.go
@@ -1,19 +1,69 @@
 package kindergarten
 
-// Define the Garden type here.
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"sort"
+	"strings"
+)
 
-// The diagram argument starts each row with a '\n'.  This allows Go's
-// raw string literals to present diagrams in source code nicely as two
-// rows flush left, for example,
-//
-//     diagram := `
-//     VVCCGG
-//     VVCCGG`
-
-func NewGarden(diagram string, children []string) (*Garden, error) {
-	panic("Please implement the NewGarden function")
+type Garden struct {
+	PlantsByChidren map[string][]string
 }
 
 func (g *Garden) Plants(child string) ([]string, bool) {
-	panic("Please implement the Plants function")
+	plants, ok := g.PlantsByChidren[child]
+	if !ok {
+		return nil, false
+	}
+	return plants, true
+}
+
+func NewGarden(diagram string, children []string) (*Garden, error) {
+	var sortedChildren = make([]string, len(children))
+	copy(sortedChildren, children)
+	sort.Strings(sortedChildren)
+
+	validationRe := regexp.MustCompile(fmt.Sprintf("(\n[RVCG]{%d}){2}", 2*len(children)))
+	if !validationRe.Match([]byte(diagram)) {
+		return nil, errors.New("wrong diagram format")
+	}
+
+	// Make diagram as one line once validated
+	diagram = strings.ReplaceAll(diagram, "\n", "")
+
+	var plants = map[string][]string{}
+	for i, child := range sortedChildren {
+		if _, ok := plants[child]; ok {
+			return nil, errors.New("duplicate user")
+		}
+
+		a := 2 * i
+		b := a + 1
+		c := 2*len(children) + a
+		d := c + 1
+
+		plants[child] = append(plants[child], Plant(string(diagram[a])))
+		plants[child] = append(plants[child], Plant(string(diagram[b])))
+		plants[child] = append(plants[child], Plant(string(diagram[c])))
+		plants[child] = append(plants[child], Plant(string(diagram[d])))
+	}
+
+	return &Garden{PlantsByChidren: plants}, nil
+}
+
+func Plant(s string) string {
+	switch s {
+	case "R":
+		return "radishes"
+	case "V":
+		return "violets"
+	case "C":
+		return "clover"
+	case "G":
+		return "grass"
+	default:
+		panic("no flower")
+	}
 }
