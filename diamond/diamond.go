793e005835f25cc6ffcad7bb14fea0c1b430d5ee
Alexandre Barret <alexandre@hatchinvest.nz>
2022-06-22 13:01:31 +1200

    Solve diamond

--- diamond/diamond.go
+++ diamond/diamond.go
@@ -9,11 +9,11 @@ func Gen(char byte) (string, error) {
 	if char < 'A' || 'Z' < char {
 		return "", errors.New("out of range")
 	}
+
 	l1 := int(char - 'A')
 	l2 := 2*l1 + 1
 
 	var result = make([]string, l2)
-
 	for i := 0; i <= l1; i++ {
 		letter := char - byte(i)
 		result[l1-i] = GenLine(letter, i, l2)
@@ -26,29 +26,9 @@ func Gen(char byte) (string, error) {
 func GenLine(char byte, pos, length int) string {
 	var result = make([]byte, length)
 	for i := range result {
-		result[i] = '·'
+		result[i] = '.'
 	}
 	result[pos] = char
 	result[length-pos-1] = char
 	return string(result)
 }
-
-// "...A..."
-// "..B.B.."
-// ".C...C."
-// "D.....D"
-// ".C...C."
-// "..B.B.."
-// "...A..."
-
-// .....A.....
-// ....B.B....
-// ...C...C...
-// ..D.....D..
-// .E.......E.
-// F.........F
-// .E.......E.
-// ..D.....D..
-// ...C...C...
-// ....B.B....
-// .....A.....
