45b5653d3ff3c4fcac23bd32ca69b809134e3b14
Alexandre Barret <alexandre@hatchinvest.nz>
2022-07-08 20:27:35 +1200

    Refactor solution

--- pascals-triangle/pascals_triangle.go
+++ pascals-triangle/pascals_triangle.go
@@ -3,14 +3,12 @@ package pascal
 func Triangle(n int) [][]int {
 	result := make([][]int, n)
 	result[0] = []int{1}
-
 	for i := 1; i < n; i++ {
-		row := make([]int, i+1)
-		row[0], row[i] = 1, 1
+		result[i] = make([]int, i+1)
+		result[i][0], result[i][i] = 1, 1
 		for j := 1; j < len(result[i-1]); j++ {
-			row[j] = result[i-1][j-1] + result[i-1][j]
+			result[i][j] = result[i-1][j-1] + result[i-1][j]
 		}
-		result[i] = row
 	}
 	return result
 }
