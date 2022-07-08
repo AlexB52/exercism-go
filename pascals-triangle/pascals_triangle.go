53547be4263ece4b29a837aeb561be683168693e
Alexandre Barret <alexandre@hatchinvest.nz>
2022-07-08 20:38:58 +1200

    Refactor again

--- pascals-triangle/pascals_triangle.go
+++ pascals-triangle/pascals_triangle.go
@@ -3,12 +3,11 @@ package pascal
 func Triangle(n int) [][]int {
 	triangle := make([][]int, n)
 	for i := 0; i < n; i++ {
-		row := make([]int, i+1)
-		row[0], row[i] = 1, 1
+		triangle[i] = make([]int, i+1)
+		triangle[i][0], triangle[i][i] = 1, 1
 		for j := 1; j < i; j++ {
-			row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+			triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
 		}
-		triangle[i] = row
 	}
 	return triangle
 }
