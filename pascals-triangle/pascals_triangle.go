23a07b54310b34d6229ce773fd387f5afaeddae6
Alexandre Barret <alexandre@hatchinvest.nz>
2022-07-08 20:11:10 +1200

    Solve pascals triangle

--- pascals-triangle/pascals_triangle.go
+++ pascals-triangle/pascals_triangle.go
@@ -1,5 +1,16 @@
 package pascal
 
 func Triangle(n int) [][]int {
-	panic("Please implement the Triangle function")
+	result := make([][]int, n)
+	result[0] = []int{1}
+
+	for i := 1; i < n; i++ {
+		row := make([]int, i+1)
+		row[0], row[i] = 1, 1
+		for j := 1; j < len(result[i-1]); j++ {
+			row[j] = result[i-1][j-1] + result[i-1][j]
+		}
+		result[i] = row
+	}
+	return result
 }
