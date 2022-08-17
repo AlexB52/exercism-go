db20ae5aa69e3d76b04ad20bed522a1d43a01c0b
Alexandre Barret <alexandre@hatchinvest.nz>
2022-08-17 13:10:35 +1200

    refactor sublist

--- sublist/sublist.go
+++ sublist/sublist.go
@@ -14,29 +14,20 @@ const (
 )
 
 func Sublist(l1, l2 []int) Relation {
-	if len(l1) != 0 && len(l2) == 0 {
-		return SuperList
-	}
-
-	if len(l1) == 0 && len(l2) != 0 {
-		return SubList
-	}
-
-	if len(l1) == len(l2) && DeepEqual(l1, l2) {
+	if DeepEqual(l1, l2) {
 		return Equal
 	}
 
 	var result Relation = Unequal
 	var reversed bool
-	var a, b = l1, l2
 
-	if len(a) < len(b) {
-		a, b = b, a
+	if len(l1) < len(l2) {
+		l1, l2 = l2, l1
 		reversed = true
 	}
 
-	for i := 0; i <= len(a)-len(b); i++ {
-		if a[i] == b[0] && DeepEqual(a[i:len(b)+i], b) {
+	for i := 0; i <= len(l1)-len(l2); i++ {
+		if DeepEqual(l1[i:len(l2)+i], l2) {
 			result = SuperList
 			if reversed {
 				result = SubList
