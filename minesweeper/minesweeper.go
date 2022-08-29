71383c6255c85e738ed4b6802121e1b206df43e3
Alexandre Barret <alexandre@hatchinvest.nz>
2022-08-29 16:06:26 +1200

    WIP -minesweeper

--- minesweeper/minesweeper.go
+++ minesweeper/minesweeper.go
@@ -1,6 +1,44 @@
 package minesweeper
 
 // Annotate returns an annotated board
+type Incrementer interface {
+	increment()
+}
+
+type Mine struct {
+	x, y  int
+	Value interface{}
+}
+
+type Position struct {
+	x, y  int
+	Value interface{}
+}
+
+func (p *Position) increment() {
+	p.Value++
+}
+
+func (p *Mine) increment() {}
+
 func Annotate(board []string) []string {
-	panic("Please implement the Annotate function")
+	if len(board) == 0 || len(board[0]) == 0 {
+		return board
+	}
+
+	matrix := make([][]int, len(board))
+	for i := 0; i < len(board); i++ {
+		matrix[i] = make([]int, len(board[0]))
+	}
+
+	for i, row := range board {
+		for j, col := range row {
+			if col != '*' {
+				continue
+			}
+			matrix[i][j]++
+		}
+	}
+
+	return board
 }
