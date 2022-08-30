c753829a6d0070c1a1920811302345461094ae45
Alexandre Barret <barretalexandre@hotmail.com>
2022-08-30 20:30:48 +1200

    Solve minesweeper

--- minesweeper/minesweeper.go
+++ minesweeper/minesweeper.go
@@ -1,34 +1,19 @@
 package minesweeper
 
+import (
+	"fmt"
+)
+
 // Annotate returns an annotated board
-type Incrementer interface {
-	increment()
-}
-
-type Mine struct {
-	x, y  int
-	Value interface{}
-}
-
-type Position struct {
-	x, y  int
-	Value interface{}
-}
-
-func (p *Position) increment() {
-	p.Value++
-}
-
-func (p *Mine) increment() {}
 
 func Annotate(board []string) []string {
 	if len(board) == 0 || len(board[0]) == 0 {
 		return board
 	}
 
-	matrix := make([][]int, len(board))
+	mineBoard := make([][]rune, len(board))
 	for i := 0; i < len(board); i++ {
-		matrix[i] = make([]int, len(board[0]))
+		mineBoard[i] = make([]rune, len(board[0]))
 	}
 
 	for i, row := range board {
@@ -36,9 +21,44 @@ func Annotate(board []string) []string {
 			if col != '*' {
 				continue
 			}
-			matrix[i][j]++
+			mineBoard[i][j] = '*'
+			Increment(mineBoard, i-1, j-1)
+			Increment(mineBoard, i-1, j)
+			Increment(mineBoard, i-1, j+1)
+			Increment(mineBoard, i, j-1)
+			Increment(mineBoard, i, j+1)
+			Increment(mineBoard, i+1, j-1)
+			Increment(mineBoard, i+1, j)
+			Increment(mineBoard, i+1, j+1)
 		}
 	}
 
-	return board
+	return PrintResult(mineBoard)
+}
+
+func Increment(board [][]rune, i, j int) {
+	x, y := len(board)-1, len(board[0])-1
+	if i > x || i < 0 || j > y || j < 0 || board[i][j] == '*' {
+		return
+	}
+	board[i][j]++
+}
+
+func PrintResult(board [][]rune) []string {
+	var result = make([]string, len(board))
+	for i, line := range board {
+		for _, char := range line {
+			var value string
+			switch char {
+			case '*':
+				value = "*"
+			case 0:
+				value = " "
+			default:
+				value = fmt.Sprint(char - 0)
+			}
+			result[i] += value
+		}
+	}
+	return result
 }
