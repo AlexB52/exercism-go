7e911c6edc621457c350ce8681df282e08552383
Alexandre Barret <barretalexandre@hotmail.com>
2022-09-01 19:43:11 +1200

    WIP - find score

--- poker/poker.go
+++ poker/poker.go
@@ -4,9 +4,24 @@ import (
 	"errors"
 	"fmt"
 	"regexp"
+	"sort"
+	"strconv"
 )
 
+type HandRank int
 type Card string
+
+const (
+	HighCard HandRank = iota
+	OnePair
+	TwoPair
+	ThreeOfAKind
+	Straight
+	Flush
+	FullHouse
+	FourOfAKind
+	StraightFlush
+)
 
 func (c Card) Suit() string {
 	return string([]rune(c)[1])
@@ -17,33 +32,62 @@ func (c Card) Value() string {
 }
 
 type Hand struct {
-	Cards []Card
+	Cards  []Card
+	Suits  map[string]int
+	Values map[string]int
+	List   []int
 }
 
-func (h Hand) Combination() string {
-	suits, values := map[string]int{}, map[string]int{}
-	for _, c := range h.Cards {
-		suits[c.Suit()]++
-		values[c.Value()]++
+func (h *Hand) Score() HandRank {
+	switch {
+	default:
+		return HighCard
 	}
-	fmt.Println("suits", suits)
-	fmt.Println("values", values)
-	return ""
 }
 
-func BestHand(hands []string) ([]string, error) {
-	re := regexp.MustCompile("\\b([2-9AJQK]|10)([♤♡♢♧])\\b")
-	for _, h := range hands {
-		cards := re.FindAllString(h, -1)
-		if len(cards) != 5 {
+func BuildHand(cards []string) Hand {
+	hand := Hand{
+		Cards:  make([]Card, 5),
+		Suits:  map[string]int{},
+		Values: map[string]int{},
+		List:   []int{},
+	}
+
+	for i, c := range cards {
+		card := Card(c)
+		hand.Cards[i] = card
+		hand.Suits[card.Suit()]++
+		hand.Values[card.Value()]++
+		i, _ := strconv.Atoi(card.Value())
+		hand.List = append(hand.List, i)
+	}
+
+	sort.Ints(hand.List)
+
+	fmt.Println("suits", hand.Suits)
+	fmt.Println("values", hand.Values)
+	fmt.Println("list", hand.List)
+	fmt.Println()
+
+	return hand
+}
+
+func BestHand(dealtHands []string) ([]string, error) {
+	cardRegex := "([2-9AJQK]|10)([♤♡♢♧])"
+
+	reValidation := regexp.MustCompile(fmt.Sprintf("^(%s\\s){4}%s$", cardRegex, cardRegex))
+	for _, h := range dealtHands {
+		if !reValidation.Match([]byte(h)) {
 			return nil, errors.New("invalid cards")
 		}
+	}
 
-		hand := Hand{}
-		for _, c := range cards {
-			hand.Cards = append(hand.Cards, Card(c))
-		}
-		hand.Combination()
+	var hands = []Hand{}
+	var highestScore HandRank
+	for _, h := range dealtHands {
+		cards := regexp.MustCompile(cardRegex).FindAllString(h, -1)
+		fmt.Printf("%q\n ", cards)
+		hands = append(hands, BuildHand(cards))
 		// fmt.Println("matches", matches)
 		// fmt.Printf("%q\n", matches)
 	}
