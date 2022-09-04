e5cb0abf1a9755ea4e489cc8d4b2f0dc24da1155
Alexandre Barret <barretalexandre@hotmail.com>
2022-09-04 16:59:36 +1100

    Solve change

--- change/change.go
+++ change/change.go
@@ -1,5 +1,32 @@
 package change
 
+import (
+	"errors"
+	"math"
+)
+
 func Change(coins []int, target int) ([]int, error) {
-	panic("Please implement the Change function")
+	coinsPerChange := map[int][]int{0: []int{}}
+
+	for i := 1; i <= target; i++ {
+		var min = math.MaxUint8
+		for _, coin := range coins {
+			previous, ok := coinsPerChange[i-coin]
+			if !ok {
+				continue
+			}
+
+			if min > len(previous) {
+				coinsPerChange[i] = append([]int{coin}, previous...)
+				min = len(previous)
+			}
+		}
+	}
+
+	result, ok := coinsPerChange[target]
+	if !ok {
+		return nil, errors.New("no change available")
+	}
+
+	return result, nil
 }
