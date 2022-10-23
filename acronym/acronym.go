527f89b8a8f6d58f7400e0ab235ceeadc19e1b14
Alex Barret <alex.barret@veridapt.com>
2022-10-23 14:14:41 +1300

    Solve acronym

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -1,15 +1,15 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package acronym should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package acronym
 
-// Abbreviate should have a comment documenting it.
+import (
+	"regexp"
+	"strings"
+)
+
 func Abbreviate(s string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	var result string
+	re := regexp.MustCompile("\\b[^a-zA-Z]?([a-zA-Z])[\\w']*\\b")
+	for _, match := range re.FindAllStringSubmatch(s, -1) {
+		result += strings.ToUpper(match[1])
+	}
+	return result
 }
