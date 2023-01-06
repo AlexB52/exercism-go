a90debcb55d15e2df0ff64ea10b344a46029957e
Alexandre Barret <barret.alx@gmail.com>
2023-01-07 10:41:45 +1300

    remove math package

--- darts/darts.go
+++ darts/darts.go
@@ -1,16 +1,14 @@
 package darts
 
-import "math"
-
 func Score(x, y float64) int {
-	distance := math.Sqrt(x*x + y*y)
+	distance := x*x + y*y
 
 	switch {
 	case distance <= 1:
 		return 10
-	case distance <= 5:
+	case distance <= 25:
 		return 5
-	case distance <= 10:
+	case distance <= 100:
 		return 1
 	default:
 		return 0
