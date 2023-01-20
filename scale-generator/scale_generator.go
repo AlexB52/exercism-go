e74a2591cbca4ea2d178cb00d53bb414bfcaa451
Alexandre Barret <barret.alx@gmail.com>
2023-01-21 12:01:53 +1300

    Refactor from community solutions

--- scale-generator/scale_generator.go
+++ scale-generator/scale_generator.go
@@ -16,51 +16,41 @@ var SIGNATURES = map[KeySignature][]string{
 	flat:  []string{"A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"},
 }
 
-func Scale(tonic, interval string) (scale []string) {
-	switch tonic {
-	case "C", "a":
-		scale = SIGNATURES[sharp]
-	case "G", "D", "A", "E", "B", "F#", "e", "b", "f#", "c#", "g#", "d#":
-		scale = SIGNATURES[sharp]
-	case "F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb":
-		scale = SIGNATURES[flat]
-	}
-
-	scale = scaleFromTonic(tonic, scale)
-	scale = selectIntervals(scale, interval)
-	return scale
-}
-
-func scaleFromTonic(tonic string, scale []string) (result []string) {
-	for i, note := range scale {
-		if strings.ToUpper(note) != strings.ToUpper(tonic) {
-			continue
-		}
-		result = scale[i:]
-		result = append(result, scale[:i]...)
-		break
-	}
-	return result
-}
-
-func selectIntervals(scale []string, interval string) []string {
+func Scale(tonic, interval string) []string {
 	if interval == "" {
 		interval = "mmmmmmmmmmm"
 	}
 
-	var stepIndex int
-	result := make([]string, len(interval)+1)
-	result[0] = scale[0]
+	signature := Signature(tonic)
+	stepIndex := TonicIndex(tonic, signature)
+
+	scale := make([]string, len(interval)+1)
+	scale[0] = signature[stepIndex]
 	for i, step := range interval {
-		switch step {
-		case 'M':
-			stepIndex += 2
-		case 'A':
-			stepIndex += 3
-		default:
-			stepIndex += 1
-		}
-		result[i+1] = scale[stepIndex%12]
+		stepIndex += map[rune]int{'m': 1, 'M': 2, 'A': 3}[step]
+		scale[i+1] = signature[stepIndex%len(signature)]
 	}
-	return result
+
+	return scale
+}
+
+func Signature(tonic string) (signature []string) {
+	switch tonic {
+	case "C", "a":
+		signature = SIGNATURES[sharp]
+	case "G", "D", "A", "E", "B", "F#", "e", "b", "f#", "c#", "g#", "d#":
+		signature = SIGNATURES[sharp]
+	case "F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb":
+		signature = SIGNATURES[flat]
+	}
+	return signature
+}
+
+func TonicIndex(tonic string, scale []string) int {
+	for i, note := range scale {
+		if strings.ToUpper(note) == strings.ToUpper(tonic) {
+			return i
+		}
+	}
+	return -1
 }
